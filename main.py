a6fa7c43635b7cc19f425105ccead0483719fa09
Sod021 <oseidonkorsamuel@gmail.com>
2025-07-19 12:26:26 +0000

    GET response removed

--- main.py
+++ main.py
@@ -26,5 +26,5 @@ def read_root():
 # For local dev
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8080)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
 
